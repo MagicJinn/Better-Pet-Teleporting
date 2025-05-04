ab1dee026e6d588c264d143258b81f729f9e3927
MagicJinn <jinn.bz@mail.com>
2025-05-04 22:33:48 +0200

    1.1

--- src/main/java/com/BetterPetTeleporting/BetterPetTeleporting.java
+++ src/main/java/com/BetterPetTeleporting/BetterPetTeleporting.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class BetterPetTeleporting {
     public static final String MODID = "betterpetteleporting";
     public static final String NAME = "Better Pet Teleporting";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static Logger logger;
 
